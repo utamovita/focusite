82a57ccb0d374e8c51a15f22bab90c4df731722f
utamovita <michalkowit@gmail.com>
2020-04-01 17:20:49 +0200

    unfinished submenu, small scss refactor

--- front-page.php
+++ front-page.php
@@ -89,7 +89,7 @@
             <div class="spacer-top"></div>
             <div class="info">
                 <ul>
-                    <li class="email"><a href="mailto:office@focusite.pl" title="mail">office@focusite.pl</a></li><li class="phone"><a href="tel:+48732009546" title="phone">+48 732 009 546</a></li><li class="address">Dworcowa 1, 64-320 Buk</li>
+                    <li class="email"><a href="mailto:office@focusite.pl">office@focusite.pl</a></li><li class="phone"><a href="tel:+48732009546">+48 732 009 546</a></li><li class="address">Dworcowa 1, 64-320 Buk</li>
                 </ul>
             </div>
         </div>
--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 function awesome_script_enqueue(){
     wp_enqueue_style('style', get_template_directory_uri() . '/style.css', array(), '', 'all');
     wp_enqueue_script('jquery');
-    wp_enqueue_script('custom', get_template_directory_uri() . '/js/custom.js', array(), '', true);
+    wp_enqueue_script('custom', get_template_directory_uri() . '/js/custom.min.js', array(), '', true);
 }
 
 add_action('wp_enqueue_scripts', 'awesome_script_enqueue');
@@ -17,6 +17,7 @@ function awesome_theme_setup(){
 
 add_action('init','awesome_theme_setup');
 
+//Remove the "type" attribut in script tag
 add_action(
     'after_setup_theme',
     function() {
@@ -24,8 +25,8 @@ add_action(
     }
 );
 
-//Page Slug Body Class
-function add_slug_body_class( $classes ) {
+//Add body slug classes
+function add_slug_body_class ($classes) {
     global $post;
     if ( isset( $post ) ) {
     $classes[] = $post->post_type . '-' . $post->post_name;
@@ -34,6 +35,36 @@ function add_slug_body_class( $classes ) {
 }
 
 add_filter( 'body_class', 'add_slug_body_class' );
+
+//Remove wp-block css
+function smartwp_remove_wp_block_library_css() {
+    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
+}
+
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css' );
+
+//Remove wp-embed js
+function my_deregister_scripts() {
+    wp_dequeue_script( 'wp-embed' );
+}
+   
+add_action( 'wp_footer', 'my_deregister_scripts' );
+
+//Remove JQuery migrate
+function remove_jquery_migrate($scripts) {
+    if (!is_admin() && isset($scripts->registered['jquery'])) {
+        $script = $scripts->registered['jquery'];
+        
+        if ($script->deps) { // Check whether the script has any dependencies
+            $script->deps = array_diff($script->deps, array(
+                'jquery-migrate'
+            ));
+        }
+    }
+}
+
+add_action('wp_default_scripts', 'remove_jquery_migrate');
 
 remove_action ('wp_head', 'rsd_link');
 remove_action( 'wp_head', 'wlwmanifest_link');
