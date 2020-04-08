7e44b9f099c23ca385950a740c716a3a0b3e8036
utamovita <michalkowit@gmail.com>
2020-04-08 04:04:11 +0200

    new pages without content

---
+++ page-sklepy-internetowe.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+<main>
+    <img src="<?php bloginfo('template_url'); ?>/img/offer-top-wave.png" class="wave-top" alt="purple background top wave">
+    <section>
+        <h1>Sklepy internetowe</h1>
+    </section>
+</main>
+<footer>
+    <div class="container">
+        <div class="lg-6">
+            <div class="spacer-top"></div>
+            <div class="info">
+                <ul>
+                    <li class="email"><a href="mailto:office@focusite.pl">office@focusite.pl</a></li><li class="phone"><a href="tel:+48732009546">+48 732 009 546</a></li><li class="address">Dworcowa 1, 64-320 Buk</li>
+                </ul>
+            </div>
+        </div>
+        <div class="lg-6 copyright">© focusite <br>All rights reserved</div>
+    </div>
+    <img src="<?php bloginfo('template_url'); ?>/img/offer-bottom-wave.png" class="bottom-wave" alt="purple background bottom wave">
+</footer>
+
+<?php get_footer(); ?>
