875eea428a6d28a460c296f00b75b7460ace4687
utamovita <michalkowit@gmail.com>
2020-04-17 20:50:20 +0200

    faq changes

--- page-faq.php
+++ page-faq.php
@@ -2,8 +2,68 @@
 
 <main>
     <img src="<?php bloginfo('template_url'); ?>/img/offer-top-wave.png" class="wave-top" alt="purple background top wave">
-    <section class="section1">
-        <h1>FAQ</h1>
+    <section class="section1 container">
+        <h1><span><span class="purple-gradient">Pytania i odpowiedzi</span></span></h1>
+        <h2>Najczęściej zadawane pytania dotyczące naszych usług. <br>Zapoznaj się z poniższym działem FAQ</h2>
+    </section>
+    <section class="section2 container">
+        <div class="collapsible">
+            <div class="collapsible__question__wrapper">
+                <div class="collapsible__question">
+                    <h4>Ile kosztuje wykonanie i utrzymanie strony internetowej?</h4>
+                    <div class="collapsible__question__answer">
+                        <p>Ceny wahają się zazwyczaj między 1000zł - 2000zł netto. Ostateczna cena kompletnej strony jest różna i zależna przede wszystkim od indywidualnych potrzeb klienta.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="collapsible__question__wrapper">
+                <div class="collapsible__question">
+                    <h4>Pytanie numer 2?</h4>
+                    <div class="collapsible__question__answer">
+                        <p>Ceny wahają się zazwyczaj między 1000zł - 2000zł netto. Ostateczna cena kompletnej strony jest różna i zależna przede wszystkim od indywidualnych potrzeb klienta.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="collapsible__question__wrapper">
+                <div class="collapsible__question">
+                    <h4>Siema eniu?</h4>
+                    <div class="collapsible__question__answer">
+                        <p>Ceny wahają się zazwyczaj między 1000zł - 2000zł netto. Ostateczna cena kompletnej strony jest różna i zależna przede wszystkim od indywidualnych potrzeb klienta.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="collapsible__question__wrapper">
+                <div class="collapsible__question">
+                    <h4>Ile kosztuje wykonanie i utrzymanie strony internetowej?</h4>
+                    <div class="collapsible__question__answer">
+                        <p>Ceny wahają się zazwyczaj między 1000zł - 2000zł netto. Ostateczna cena kompletnej strony jest różna i zależna przede wszystkim od indywidualnych potrzeb klienta.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="collapsible__question__wrapper">
+                <div class="collapsible__question">
+                    <h4>Ile kosztuje wykonanie i utrzymanie strony internetowej?</h4>
+                    <div class="collapsible__question__answer">
+                        <p>Ceny wahają się zazwyczaj między 1000zł - 2000zł netto. Ostateczna cena kompletnej strony jest różna i zależna przede wszystkim od indywidualnych potrzeb klienta.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="collapsible__question__wrapper">
+                <div class="collapsible__question">
+                    <h4>Ile kosztuje wykonanie i utrzymanie strony internetowej?</h4>
+                    <div class="collapsible__question__answer">
+                        <p>Ceny wahają się zazwyczaj między 1000zł - 2000zł netto. Ostateczna cena kompletnej strony jest różna i zależna przede wszystkim od indywidualnych potrzeb klienta.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section3">
+        <div class="info-box">
+            <h2>Nie znalazłeś odpowiedzi na <span><span class="orange-gradient">swoje pytanie?</span></span></h2>
+            <h5>Odezwij się do nas, na pewno pomożemy.</h5>
+            <a href="kontakt" title="kontakt" class="btn orange empty">Skontaktuj się</a><span class="or">lub</span><a href="wycena" class="btn orange" title="brief">Zapytaj o wycenę</a>
+        </div>
     </section>
 </main>
 
--- page-sklepy-internetowe.php
+++ page-sklepy-internetowe.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>
 
-<main>
+<main class="offer-subpage">
     <img src="<?php bloginfo('template_url'); ?>/img/offer-top-wave.png" class="wave-top" alt="purple background top wave">
-    <section>
-        <h1>Sklepy internetowe</h1>
+    <section class="section1">
+        <h1><span><span class="purple-gradient">Sklepy internetowe</span></span></h1>
+        <h2>Najczęściej zadawane pytania dotyczące naszych usług. <br>Zapoznaj się z poniższym działem FAQ</h2>
     </section>
 </main>
 
