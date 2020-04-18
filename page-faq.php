565f61aaba2797b2e21462f483b16c6f0bd03437
utamovita <michalkowit@gmail.com>
2020-04-18 20:11:27 +0200

    faq some questions + styles

--- page-faq.php
+++ page-faq.php
@@ -12,47 +12,39 @@
                 <div class="collapsible__question">
                     <h4>Ile kosztuje wykonanie i utrzymanie strony internetowej?</h4>
                     <div class="collapsible__question__answer">
-                        <p>Ceny wahają się zazwyczaj między 1000zł - 2000zł netto. Ostateczna cena kompletnej strony jest różna i zależna przede wszystkim od indywidualnych potrzeb klienta.</p>
+                        <p>Cena wykonania strony internetowej waha się zazwyczaj między <strong>1000zł - 2000zł netto</strong>. Ostateczna cena kompletnej strony jest różna i zależna przede wszystkim od indywidualnych potrzeb klienta oraz stopnia ich zaawansowania. Koszt utrzymania serwisu www ogranicza się u nas tylko do wykupienia domeny (jedynie <strong>99zł</strong> netto/rok). Jeśli chciałbyś poznać orientacyjną cenę swojego pomysłu na serwis www, wypełnij <a href="wycena">krótki brief</a>, a w ciągu doby otrzymasz odpowiedź mailem.</p>
                     </div>
                 </div>
             </div>
             <div class="collapsible__question__wrapper">
                 <div class="collapsible__question">
-                    <h4>Pytanie numer 2?</h4>
+                    <h4>Czy moja strona będzie się dobrze wyświetlać na telefonie lub tablecie?</h4>
                     <div class="collapsible__question__answer">
-                        <p>Ceny wahają się zazwyczaj między 1000zł - 2000zł netto. Ostateczna cena kompletnej strony jest różna i zależna przede wszystkim od indywidualnych potrzeb klienta.</p>
+                        <p>Tak, wszystkie strony jakie tworzymy są dostowane do obsługi na urządzeniach mobilnych. <strong>Responsywność</strong> (RWD - ang. Responsive Web Design), czyli dopasowanie odpowiedniego układu, wyglądu i wielkości elementów strony w zależności od używanego urządzenia to podstawowa cecha <strong>profesjonalnej</strong> strony internetowej. Oprócz tego, responsywność jest dodatkowo czynnikiem branym pod uwagę przez algorytmy wyszukiwarkę Google, dzięki czemu strona ma szansę na jeszcze wyższy wynik.</p>
                     </div>
                 </div>
             </div>
             <div class="collapsible__question__wrapper">
                 <div class="collapsible__question">
-                    <h4>Siema eniu?</h4>
+                    <h4>Czy będę mógł/mogła samodzielnie zmieniać treść i zdjęcia na mojej stronie?</h4>
                     <div class="collapsible__question__answer">
-                        <p>Ceny wahają się zazwyczaj między 1000zł - 2000zł netto. Ostateczna cena kompletnej strony jest różna i zależna przede wszystkim od indywidualnych potrzeb klienta.</p>
+                        <p>Tak, o ile zdecydujesz się na stronę z systemem zarządzania treścią. <strong>CMS</strong> (ang. Content Management System) to funkcjonalność, dzięki której możesz samodzielnie edytować zawartość strony bez znajomości programowania. Każdy klient może zdecydować się na taką możliwość <strong>na dowolnym etapie współpracy</strong> (również po wykonanej stronie). Planujesz posiadać na stronie bloga lub regularnie edytować treść strony? Zdecydowanie opcja dla Ciebie. Natomiast, jeżeli planujesz rzadko wprowadzać zmiany na stronie i nie chcesz dopłacać za tę funkcjonalność, zrobimy to za Ciebie. </p>
                     </div>
                 </div>
             </div>
             <div class="collapsible__question__wrapper">
                 <div class="collapsible__question">
-                    <h4>Ile kosztuje wykonanie i utrzymanie strony internetowej?</h4>
+                    <h4>Co to jest pozycjonowanie (SEO) ?</h4>
                     <div class="collapsible__question__answer">
-                        <p>Ceny wahają się zazwyczaj między 1000zł - 2000zł netto. Ostateczna cena kompletnej strony jest różna i zależna przede wszystkim od indywidualnych potrzeb klienta.</p>
+                        <p><strong>SEO (ang. Search Engine Optimization)</strong> - krótko mówiąc jest to zespół działań mających na celu osiągnięcie jak najwyższych pozycji w wyszukiwarce (np. Google). Jest to jeden z <strong>najbardziej efektywnych</strong> sposobów marketingu internetowego. Wysoka pozycja w wynikach wyszukiwania ma niezwykle duże znaczenie w budowaniu rozpoznawalności marki. Przeciętny użytkownik widząc pierwszą stronę wyników Google uważą ją za zaufaną i najbardziej adekwatną do jego zapytania.</p>
                     </div>
                 </div>
             </div>
             <div class="collapsible__question__wrapper">
                 <div class="collapsible__question">
-                    <h4>Ile kosztuje wykonanie i utrzymanie strony internetowej?</h4>
+                    <h4>Czy moja strona będzie widoczna w wyszukiwarce Google?</h4>
                     <div class="collapsible__question__answer">
-                        <p>Ceny wahają się zazwyczaj między 1000zł - 2000zł netto. Ostateczna cena kompletnej strony jest różna i zależna przede wszystkim od indywidualnych potrzeb klienta.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="collapsible__question__wrapper">
-                <div class="collapsible__question">
-                    <h4>Ile kosztuje wykonanie i utrzymanie strony internetowej?</h4>
-                    <div class="collapsible__question__answer">
-                        <p>Ceny wahają się zazwyczaj między 1000zł - 2000zł netto. Ostateczna cena kompletnej strony jest różna i zależna przede wszystkim od indywidualnych potrzeb klienta.</p>
+                        <p>Tak, wszystkie wykonywane przez nas strony są widoczne w Google. W ramach umowy na wykonanie strony lub sklepu internetowego <strong>zobowiązujemy się</strong> do wykonania podstawowej optimalizacji SEO, która pozwala na osiągnięcie wysokich pozycji w wyszukiwarce Google. Jeśli twoja działalność znajduje się na niewielkim, lokalnym rynku bądź w niszowej branży to taka optymalizacja może wystarczyć do znalezienia się na <strong>pierwszej stronie wyników Google</strong>. Jeśli w Twoim obszarze występuje silna konkurecja, konieczne będą dodatkowe działania z usług SEO.</p>
                     </div>
                 </div>
             </div>
